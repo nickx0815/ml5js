82635b46fc8570efcfe01d334846f5b773e720e9
nickx0815 <nick.herold96@web.de>
2020-02-14 16:27:37 +0100

    some stuff

--- website/SketchP5.js
+++ website/SketchP5.js
@@ -4,8 +4,9 @@ let poseNet;
 let poses = [];
 let label = [];
 let classifier;
-var  sendSounds
-var  sendPoses
+var sendSounds
+var sendPoses
+
 function setup() {
     frameRate(24);
     createCanvas(640, 480);
@@ -16,12 +17,11 @@ function setup() {
     sendSounds = document.getElementById("sendPosesid").checked
     sendPoses = document.getElementById("sendSoundid").checked
     initPoseNet()
-    // Create a new poseNet method with a single detection
 
 }
 
 function initPoseNet() {
-    sendSounds  = document.getElementById("sendSoundid").checked
+    sendSounds = document.getElementById("sendSoundid").checked
     sendPoses = document.getElementById("sendPosesid").checked
     var imageScaleFactorid = parseInt(document.getElementById("imageScaleFactorid").value)
     var outputStrideid = parseInt(document.getElementById("outputStrideid").value)
@@ -30,14 +30,12 @@ function initPoseNet() {
     var scoreThresholdid = parseInt(document.getElementById("scoreThresholdid").value)
     var nmsRadiusid = parseInt(document.getElementById("nmsRadiusid").value)
     var multiplierid = parseInt(document.getElementById("multiplierid").value)
-  var detectionTypeid = document.getElementById("detectionTypeid").value
-   var flipHorizontalid = document.getElementById("flipHorizontalid").value
-  if (flipHorizontalid == 'True') {
-    flipHorizontalid = true
-  }
-  else{
-    flipHorizontalid = false
-  }
+    var detectionTypeid = document.getElementById("detectionTypeid").value
+     if (document.getElementById("flipHorizontalid").checked){
+        flipHorizontalid = true
+    } else {
+        flipHorizontalid = false
+    }
 
     let options = {
         imageScaleFactor: imageScaleFactorid,
@@ -50,16 +48,10 @@ function initPoseNet() {
         detectionType: detectionTypeid,
         multiplier: multiplierid,
     }
-
-
-
     poseNet = ml5.poseNet(video, modelReady, options);
-    // This sets up an event that fills the global variable "poses"
-    // with an array every time new poses are detected
     poseNet.on('pose', function (results) {
         poses = results;
     });
-    // Hide the video element, and just show the canvas
     video.hide();
 }
 
@@ -72,8 +64,6 @@ function gotResult(error, results) {
         console.error(error);
         return;
     }
-    // The results are in an array ordered by confidence.
-    // console.log(results[0]);
     label = results.slice(0, 3)
 }
 
@@ -83,16 +73,11 @@ function draw() {
     drawSkeleton();
 }
 
-// A function to draw ellipses over the detected keypoints
 function drawKeypoints() {
-    // Loop through all the poses detected
     for (let i = 0; i < poses.length; i++) {
-        // For each pose detected, loop through all the keypoints
         let pose = poses[i].pose;
         for (let j = 0; j < pose.keypoints.length; j++) {
-            // A keypoint is an object describing a body part (like rightArm or leftShoulder)
             let keypoint = pose.keypoints[j];
-            // Only draw an ellipse is the pose probability is bigger than 0.2
             if (keypoint.score > 0.2) {
                 fill(255, 0, 0);
                 noStroke();
@@ -102,12 +87,9 @@ function drawKeypoints() {
     }
 }
 
-// A function to draw the skeletons
 function drawSkeleton() {
-    // Loop through all the skeletons detected
     for (let i = 0; i < poses.length; i++) {
         let skeleton = poses[i].skeleton;
-        // For every skeleton, loop through all body connections
         for (let j = 0; j < skeleton.length; j++) {
             let partA = skeleton[j][0];
             let partB = skeleton[j][1];
