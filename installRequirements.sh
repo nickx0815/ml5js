e86f2a10a1646717d7df3edbc3c026a3ed3b691a
nickx0815 <nick.herold96@web.de>
2020-02-14 16:48:52 +0100

    added requirements sh script

---
+++ installRequirements.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+echo "installing Websockets"
+echo ""
+pip3 pip3 install websockets --user
+echo ""
+echo "installing Selenium"
+echo ""
+pip3 install selenium --user
+echo ""
+echo "installing PyAutoGUI"
+echo ""
+pip3 pip3 install PyAutoGUI --user
+echo ""
+echo "Finished"
